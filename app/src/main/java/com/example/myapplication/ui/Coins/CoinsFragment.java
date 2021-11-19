d6db9c57952ad3c7e4d6fac0f035f5a98f2dd77d
Liarddead <alexe-aksenov@yandex.ru>
2021-11-19 18:47:19 +0300

    В разделе "Монеты" реализованны переходы на соответсвующие фрагменты по кнопкам.

--- app/src/main/java/com/example/myapplication/ui/Coins/CoinsFragment.java
+++ app/src/main/java/com/example/myapplication/ui/Coins/CoinsFragment.java
@@ -3,15 +3,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentCoinsBinding;
-import com.example.myapplication.ui.AntiochCoins.AntiochFragment;
 
 public class CoinsFragment extends Fragment {
     private FragmentCoinsBinding binding;
@@ -28,35 +27,34 @@ public class CoinsFragment extends Fragment {
         JerusalimButton = (ImageButton) root.findViewById(R.id.Jerusalim_button);
         TripoliButton = (ImageButton) root.findViewById(R.id.Tripoli_button);
         TeutonicButton= (ImageButton) root.findViewById(R.id.teutonic_button);
-        AntiochButton.setOnClickListener(new View.OnClickListener() {
+       AntiochButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
+           public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_nav_gallery_to_antiochFragment);
             }
         });
         JerusalimButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Navigation.findNavController(view).navigate(R.id.action_nav_gallery_to_jerusalemFragment);
             }
         });
         TripoliButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Navigation.findNavController(view).navigate(R.id.action_nav_gallery_to_tripoliFragment);
             }
         });
         TeutonicButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Navigation.findNavController(view).navigate(R.id.action_nav_gallery_to_teutonicFragment);
             }
         });
 
         return root;
 
     }
-
     @Override
     public void onDestroyView() {
         super.onDestroyView();
---
+++ app/src/main/java/com/example/myapplication/ui/TeutonicCoins/TeutonicFragment.java
@@ -0,0 +1,53 @@
+package com.example.myapplication.ui.TeutonicCoins;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.R;
+public class TeutonicFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public TeutonicFragment() {
+        // Required empty public constructor
+    }
+    // TODO: Rename and change types and number of parameters
+    public static TeutonicFragment newInstance(String param1, String param2) {
+        TeutonicFragment fragment = new TeutonicFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Teutonic Coins");
+        return inflater.inflate(R.layout.fragment_teutonic, container, false);
+    }
+}
